cd70681d08e8574cf20321dda9ca93d925ebe72f
mickw <mickw@6532b1b7-620d-0410-ab43-be6b97227153>
2006-05-02 15:20:50 +0000

    SEARCH-578

--- commons-ioc/trunk/src/main/java/no/schibstedsok/common/ioc/BasicInvocationHandler.java
+++ commons-ioc/trunk/src/main/java/no/schibstedsok/common/ioc/BasicInvocationHandler.java
@@ -29,26 +29,26 @@ import org.apache.log4j.Logger;
  */
 final class BasicInvocationHandler implements InvocationHandler {
 
-    private final Map<Method,Map<List<Class>,Method>> METHOD_CACHE = new HashMap<Method,Map<List<Class>,Method>>();
-    private final Map<Method,Map<List<Class>,BaseContext>> CONTEXT_CACHE = new HashMap<Method,Map<List<Class>,BaseContext>>();
-    
+    private final Map<Method,Map<List<Class>,Method>> methodCache = new HashMap<Method,Map<List<Class>,Method>>();
+    private final Map<Method,Map<List<Class>,BaseContext>> contextCache = new HashMap<Method,Map<List<Class>,BaseContext>>();
+
    // Attributes ----------------------------------------------------
 
     private final List<BaseContext> contexts;
 
    // Static --------------------------------------------------------
-    
+
     private static final Logger LOG = Logger.getLogger(BasicInvocationHandler.class);
-    
+
     private static final String ERR_METHOD_NOT_IN_INTERFACE = "Unable to proxy to the contexts associated to this BasicInvocationHandler for ";
     private static final String ERR_METHOD_NOT_IN_EXACT_INTERFACE = "No exact signature to the contexts associated to this BasicInvocationHandler for ";
     private static final String DEBUG_LOOKING_FOR = " Looking for ";
     private static final String DEBUG_LOOKING_IN = "Looking in ";
     private static final String DEBUG_FOUND = "Found method while: ";
-    private static final String DEBUG_NOT_FOUND = "Did not found method while: ";    
+    private static final String DEBUG_NOT_FOUND = "Did not found method while: ";
     private static final String DEBUG_ADD_TO_CACHE = "Adding to cache ";
     private static final String DEBUG_CACHE_USED_FOR = "Using cache for ";
-            
+
    // Constructors --------------------------------------------------
 
     /**
@@ -82,24 +82,24 @@ final class BasicInvocationHandler implements InvocationHandler {
         boolean paramsNotNull = true;
         // construct method's parameter signature
         final List<Class> paramSignature = new ArrayList<Class>();
-        if( objArr != null ){
+        if(objArr != null){
             for (Object obj : objArr) {
-                paramSignature.add(obj == null ? null : obj.getClass() );
+                paramSignature.add(obj == null ? null : obj.getClass());
                 paramsNotNull &= obj != null;
             }
         }
-        
+
         // This is one of the applications performance hotspots.
         //  It is a benefit to keep a cache to remember what method to use.
         final Method cachedMethod = checkCache(method, paramSignature);
-        if( cachedMethod != null ){
+        if(cachedMethod != null){
             LOG.trace(DEBUG_CACHE_USED_FOR + method);
             return invoke(cachedMethod, getContextFromCache(method,paramSignature), objArr);
         }
 
         // first pass is to find an exact signature match, we can skip if any of the params were null.
-        if( paramsNotNull ){            
-            try{ 
+        if(paramsNotNull){
+            try{
                 return invokeExactSignature(object, method, paramSignature, objArr);
 
             }  catch (NoSuchMethodException ex) {
@@ -117,7 +117,7 @@ final class BasicInvocationHandler implements InvocationHandler {
    // Protected -----------------------------------------------------
 
    // Private -------------------------------------------------------
-    
+
     /** Look for an exact signature method match to the argument objects passed in.
      **/
     private Object invokeExactSignature(
@@ -127,7 +127,7 @@ final class BasicInvocationHandler implements InvocationHandler {
             final Object[] objArr) throws Throwable {
 
         for (BaseContext cxt : contexts) {
-            
+
             final Class cls = cxt.getClass();
             try {
 
@@ -153,7 +153,7 @@ final class BasicInvocationHandler implements InvocationHandler {
         }
         throw new NoSuchMethodException(ERR_METHOD_NOT_IN_EXACT_INTERFACE + method.getName());
     }
-    
+
     /** Look for an signature matching any superclasses to the argument objects passed in.
      **/
     private Object invokeSubclassedSignature(
@@ -161,7 +161,7 @@ final class BasicInvocationHandler implements InvocationHandler {
             final Method method,
             final List<Class> paramSignature,
             final Object[] objArr) throws Throwable {
-        
+
         for (BaseContext cxt : contexts) {
 
             final Class cls = cxt.getClass();
@@ -180,10 +180,10 @@ final class BasicInvocationHandler implements InvocationHandler {
                                 + DEBUG_LOOKING_IN + cls.getName()
                                 + DEBUG_LOOKING_FOR + method.getName() + toString(paramSignature));
                         }
-                        
+
                         addToCache(method, paramSignature, m, cxt);
                         return invoke(m, cxt, objArr);
-                        
+
                     }else{
                         if(LOG.isTraceEnabled()){
                             LOG.trace(DEBUG_NOT_FOUND
@@ -198,74 +198,74 @@ final class BasicInvocationHandler implements InvocationHandler {
         LOG.error("",e);
         throw e;
     }
-    
+
     /** A method has been found in a BaseContext.
      * Invoke it.
      **/
     private Object invoke(
-            final Method method, 
-            final BaseContext cxt, 
-            final Object[] objArr) 
+            final Method method,
+            final BaseContext cxt,
+            final Object[] objArr)
                 throws IllegalAccessException, InvocationTargetException{
-        
+
         method.setAccessible(true);
         return method.invoke(cxt, objArr);
     }
-    
+
     /** Check the cache incase this method has already been called once.
      ***/
     private Method checkCache(final Method method, final List<Class> paramSignature){
-        
+
         Method cachedMethod = null;
-        final Map<List<Class>,Method> map = METHOD_CACHE.get(method);
-        if( map != null ){
+        final Map<List<Class>,Method> map = methodCache.get(method);
+        if(map != null){
             cachedMethod = map.get(paramSignature);
         }
         return cachedMethod;
     }
-    
+
     /** Get from the cache the BaseContext the method comes from.
      **/
     private BaseContext getContextFromCache(final Method method, final List<Class> paramSignature){
-        
+
         BaseContext cachedContext = null;
-        final Map<List<Class>,BaseContext> map = CONTEXT_CACHE.get(method);
-        if( map != null ){
+        final Map<List<Class>,BaseContext> map = contextCache.get(method);
+        if(map != null){
             cachedContext = map.get(paramSignature);
         }
         return cachedContext;
     }
-    
+
     /** Add to the cache the methodTo and contextTo to use for calls for methodFrom and paramSignature.
      ***/
     private void addToCache(
-            final Method methodFrom, 
-            final List<Class> paramSignature, 
+            final Method methodFrom,
+            final List<Class> paramSignature,
             final Method methodTo,
             final BaseContext contextTo){
-        
+
         LOG.trace(DEBUG_ADD_TO_CACHE + methodTo);
-        
-        Map<List<Class>,Method> methodMap = METHOD_CACHE.get(methodFrom);
-        if( methodMap == null ){
+
+        Map<List<Class>,Method> methodMap = methodCache.get(methodFrom);
+        if(methodMap == null){
             methodMap = new HashMap<List<Class>,Method>();
-            METHOD_CACHE.put(methodFrom, methodMap);
+            methodCache.put(methodFrom, methodMap);
         }
         methodMap.put(paramSignature, methodTo);
-        
-        Map<List<Class>,BaseContext> contextMap = CONTEXT_CACHE.get(methodFrom);
-        if( contextMap == null ){
+
+        Map<List<Class>,BaseContext> contextMap = contextCache.get(methodFrom);
+        if(contextMap == null){
             contextMap = new HashMap<List<Class>,BaseContext>();
-            CONTEXT_CACHE.put(methodFrom, contextMap);
+            contextCache.put(methodFrom, contextMap);
         }
         contextMap.put(paramSignature, contextTo);
-        
+
     }
-    
+
     /** Get a string representation of paramSignature.
      **/
     private String toString(final List<Class> paramSignature){
-        
+
         final StringBuilder sb = new StringBuilder();
         for (Class cls : paramSignature) {
             sb.append(cls == null ? "null" : cls.getSimpleName() + ", ");
