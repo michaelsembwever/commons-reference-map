1045b6f125808d81131134944baadf1bf1b0089a
ssenrogn <ssenrogn@6532b1b7-620d-0410-ab43-be6b97227153>
2007-04-27 07:29:50 +0000

    Improved testcases.

--- trunk/commons-validation/src/test/java/no/schibstedsok/commons/validators/hibernate/PhoneValidatorTest.java
+++ trunk/commons-validation/src/test/java/no/schibstedsok/commons/validators/hibernate/PhoneValidatorTest.java
@@ -22,38 +22,86 @@ public class PhoneValidatorTest extends TestCase {
     private static final Logger LOG = Logger.getLogger(PhoneValidatorTest.class);
 
     /**
+     * Test phone numbers with 5 digits.
+     */
+    public void testPhoneNO5() {
+        LOG.debug("## testPhoneNO5 ##");
+        final PhoneValidator validator = getValidator(PhoneType.PHONE_NO_5);
+
+        assertTrue(validator.isValid("+4702345"));
+        assertTrue(validator.isValid("004708776"));
+        assertTrue(validator.isValid("04556"));
+        assertTrue(validator.isValid("0 45 56"));
+
+        assertFalse(validator.isValid("+4734232324"));
+        assertFalse(validator.isValid("+478121"));
+        assertFalse(validator.isValid("04788121"));
+    }
+
+    /**
+     * Test phone numbers with 8 digits.
+     */
+    public void testPhoneNO8() {
+        LOG.debug("## testPhoneNO8 ##");
+        final PhoneValidator validator = getValidator(PhoneType.PHONE_NO_8);
+
+        assertTrue(validator.isValid("+4798203016"));
+        assertTrue(validator.isValid("004798203016"));
+        assertTrue(validator.isValid("98203016"));
+        assertTrue(validator.isValid("+4761171265"));
+        assertTrue(validator.isValid("61171265"));
+        assertTrue(validator.isValid("61 17 12 65"));
+
+        assertFalse(validator.isValid("01171265"));
+        assertFalse(validator.isValid("+4798121200000"));
+        assertFalse(validator.isValid("+4798121"));
+        assertFalse(validator.isValid("04798203016"));
+    }
+
+    /**
      * Test phone numbers.
      */
     public void testPhoneNO() {
-        final PhoneValidator validator = new PhoneValidator();
-
-        validator.initialize(new Phone() {
-            public String message() {
-                return null;
-            }
-
-            public PhoneType phoneType() {
-                return PhoneType.PHONE_NO;
-            }
-
-            public Class<? extends Annotation> annotationType() {
-                return null;
-            }
-        });
+        LOG.debug("## testPhoneNO ##");
+        final PhoneValidator validator = getValidator(PhoneType.PHONE_NO);
 
         assertTrue(validator.isValid("+4798203016"));
+        assertTrue(validator.isValid("004798203016"));
         assertTrue(validator.isValid("98203016"));
         assertTrue(validator.isValid("+4761171265"));
         assertTrue(validator.isValid("61171265"));
 
         assertFalse(validator.isValid("+4798121200000"));
-        assertFalse(validator.isValid("+4798121"));
+        assertFalse(validator.isValid("+47981217"));
+
+        assertTrue(validator.isValid("+4702345"));
+        assertTrue(validator.isValid("004708776"));
+        assertTrue(validator.isValid("04556"));
+
+        assertFalse(validator.isValid("+47342323248"));
+        assertFalse(validator.isValid("+478121"));
+        assertFalse(validator.isValid("04788121"));
     }
 
     /**
      * Test mobile numbers.
      */
     public void testMobileNO() {
+        LOG.debug("## testMobileNO ##");
+        final PhoneValidator validator = getValidator(PhoneType.MOBILE_NO);
+
+        assertTrue(validator.isValid("+4798203016"));
+        assertTrue(validator.isValid("004798203016"));
+        assertTrue(validator.isValid("98203016"));
+        assertTrue(validator.isValid(" 982 03 016 "));
+
+        assertFalse(validator.isValid("+4761171265"));
+        assertFalse(validator.isValid("61171265"));
+        assertFalse(validator.isValid("+4798121200000"));
+        assertFalse(validator.isValid("+4798121"));
+    }
+
+    private PhoneValidator getValidator(final PhoneType phoneType) {
         final PhoneValidator validator = new PhoneValidator();
 
         validator.initialize(new Phone() {
@@ -62,7 +110,7 @@ public class PhoneValidatorTest extends TestCase {
             }
 
             public PhoneType phoneType() {
-                return PhoneType.MOBILE_NO;
+                return phoneType;
             }
 
             public Class<? extends Annotation> annotationType() {
@@ -70,13 +118,7 @@ public class PhoneValidatorTest extends TestCase {
             }
         });
 
-        assertTrue(validator.isValid("+4798203016"));
-        assertTrue(validator.isValid("98203016"));
-
-        assertFalse(validator.isValid("+4761171265"));
-        assertFalse(validator.isValid("61171265"));
-        assertFalse(validator.isValid("+4798121200000"));
-        assertFalse(validator.isValid("+4798121"));
+        return validator;
     }
 
 }
