cbc8f01542efcd64f1c5d530147e60a2cede414e
ssmiweve <ssmiweve@6532b1b7-620d-0410-ab43-be6b97227153>
2009-01-21 16:25:47 +0000

    apply license headers to all files

--- commons-validation/trunk/src/main/java/no/schibstedsok/commons/formats/DateTimeFormatNorway.java
+++ commons-validation/trunk/src/main/java/no/schibstedsok/commons/formats/DateTimeFormatNorway.java
@@ -1,5 +1,19 @@
 /*
  * Copyright (2005-2007) Schibsted Søk AS
+ * This file is part of Sesat Commons.
+ *
+ *   Sesat Commons is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Sesat Commons is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Sesat Commons.  If not, see <http://www.gnu.org/licenses/>.
  */
 package no.schibstedsok.commons.formats;
 
